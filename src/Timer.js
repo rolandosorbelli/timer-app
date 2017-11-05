375f51a1fc4ea0dbaa0a1b979ff8c7b82ba5a27f
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-05 13:39:54 +0000

    Created a constructor with a timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class Timer extends Component {
+	constructor(props){
+		super(props);
+	
+		this.state = {
+			clock:0
+		}
+	}
   render() {
     return (
       <div>
       	<p>You have been on this site since: </p> <br />
-      	<span>55.98</span>
-      	<p>seconds.</p>
+      	<span>{this.state.clock}</span>
+      	<p>seconds</p>
       </div>
     );
   }
